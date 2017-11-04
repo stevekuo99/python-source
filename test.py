71f95b039c0d71184c66b1c50736b962f50c7a74
Steve Kuo <stevekuo99@gmail.com>
2017-11-04 20:57:56 +0800

    no message

---
+++ test.py
@@ -0,0 +1,18 @@
+
+
+"""test
+
+Note when comparing these calendars to the ones printed by cal(1): By
+default, these calendars have Monday as the first day of the week, and
+Sunday as the last (the European convention). Use setfirstweekday() to
+set the first day of the week (0=Monday, 6=Sunday)."""
+
+import sys
+import datetime
+import locale as _locale
+from itertools import repeat
+import numpy as py1
+
+
+
+    
