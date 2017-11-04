cc73c4ae0ddb4764acae6458e334a587899bcfd9
Steve Kuo <stevekuo99@gmail.com>
2017-11-04 21:33:04 +0800

    no message

--- test.py
+++ test.py
@@ -1,20 +1,22 @@
 
-
-"""test
-
-Note when comparing these calendars to the ones printed by cal(1): By
-default, these calendars have Monday as the first day of the week, and
-Sunday as the last (the European convention). Use setfirstweekday() to
-set the first day of the week (0=Monday, 6=Sunday)."""
-
-import sys
-import datetime
-import locale as _locale
-from itertools import repeat
 import numpy as np
-print(np.sin(0))
-    
+import matplotlib.pyplot as plt
+p = [10, 30, 40]
+c = 2
+for k in p:
+    print(k*c)
+result = []
+for k in p:
+    twd = k*c
+    result.append(twd)
+print(result)    
+prices = np.array(p)
+print(prices)
+print(prices * c)
 
-
-
-    
+grades = np.array([85, 70, 82])
+weights = np.array([0.3, 0.4, 0.3])
+g = grades * weights
+print(g)
+print(g.sum())
+print(np.dot(grades, weights))
